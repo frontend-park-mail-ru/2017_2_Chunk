119f5204a3ca42ea194487f0da1a94e4e92cdb20
Dmitriy Trubnikov <trubnikovdv@mail.ru>
2017-09-11 00:49:13 +0100

    'tested' sign up

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,20 +1,52 @@
 "use strict";
 
+function auth(username, password, callback) {
+
+    let xhr = new XMLHttpRequest();
+
+    // Обработчик ответа
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState !== 4) return;
+
+        if (+xhr.status !== 200) { callback(xhr, null); }
+        else { callback(null, xhr); }
+    };
+
+    // Формирование POST запроса:
+    xhr.open('POST', '/sign_up', true);     // заголовок запроса
+    xhr.setRequestHeader('Content-Type',
+        'application/json; charset=utf8');  // Content-Type для JSON
+    xhr.withCredentials = true;             // Чтобы можно было получить куки
+    xhr.timeout = 15000;                    // Время ожидания ответа от сервера
+    xhr.send(JSON.stringify({
+        username: username,
+        password: password
+    }));                                    // Отправка запроса с телом запроса
+
+}
 
 window.onload = function() {
 
-    let pageMain = document.getElementsByClassName('page panel main')[0];
-    let buttonsMenu = document.getElementsByClassName('button');
-    let buttonBack = {
+    const pageMain = document.getElementsByClassName('page panel main')[0];
+    const buttonsMenu = document.getElementsByClassName('button');
+    const buttonBack = {
         button: document.getElementsByClassName('navigation back')[0],
         currentPage: undefined
+    };
+    const signUpForm = {
+        username: document.querySelector('table.sign_up_form input[name=username]'),
+        password: document.querySelector('table.sign_up_form input[name=password]'),
+        confirm: document.querySelector('table.sign_up_form input[name=confirm]')
     };
 
     // Настройка переходов по кнопкам меню
     for (let i = 0; i < buttonsMenu.length; ++i) {
         buttonsMenu[i].addEventListener('click', event => {
             let new_page = document.getElementsByClassName(event.target.name)[0];
-            if (!new_page) return;
+            if (!new_page) {
+                console.error("Can't find class name %s", event.target.name);
+                return;
+            }
             buttonBack.currentPage = new_page;
             buttonBack.button.hidden = false;
             new_page.hidden = false;
@@ -26,5 +58,45 @@ window.onload = function() {
         buttonBack.currentPage.hidden = true;
         buttonBack.button.hidden = true;
         pageMain.hidden = false;
-    }, false)
+    }, false);
+
+    const signForm = document.querySelector('input[type=submit][name=sign_up_submit]');
+    signForm.addEventListener('click', event => {
+        // Отменяем отправку POST запроса от браузера
+        event.preventDefault();
+
+        const username = signUpForm.username.value;
+        const password = signUpForm.password.value;
+        const confirm = signUpForm.confirm.value;
+
+        // Валидация
+        let errorMessage = function(text) {
+                alert(text);
+                signUpForm.password.value = signUpForm.confirm.value = '';
+            };
+        if (password !== confirm) {
+                errorMessage("Пароли не совпадают!");
+                return;
+            }
+        if (password.length < 0) {
+                errorMessage("Длина пароля должна быть не меньше 6 символов!");
+                return;
+            }
+        if (username.length < 0) {
+                errorMessage("Длина логина должна быть не меньше 6 символов!");
+                return;
+            }
+
+        // Отправка POST запроса
+        auth(username, password, function (error, response) {
+            if (!response && error) {
+                errorMessage(error.status + ': ' + error.statusText);
+                return;
+            }
+            if (response && !error) {
+                errorMessage('Вы успешно зарегистрировались!')
+            }
+        });
+
+    }, false);
 };
--- server/index.js
+++ server/index.js
@@ -1,11 +1,52 @@
 'use strict';
 
-const express = require('express');
+const express = require('express');             // Веб-фреймворк
+const morgan = require('morgan');               // Выводит информацию о пришедшем запросе в консоль
+const bodyParser = require('body-parser');      // Парсер для тела запроса
+const cookieParser = require('cookie-parser');  // Парсер для куков
+const idCreator = require('uuid/v4');           // Создает уникальные id
 const app = express();
 
-app.use(express.static('./public'));
+
+// Middleware
+// app.use(morgan('dev'));                  // Формат выводимой инфы о запросах
+app.use(express.static('./public'));        // Отдаёт статику при совпадении имён
+app.use(bodyParser.json());                 // С помощью какой-то древней магии парсит тело запроса,
+app.use(cookieParser());                    // всё то же волшебство, но уже для кук
+
+let users = {};
+let ids = {};
+
 app.get('/', (request, response) => {
     response.send("<h2><i>Unknown page</i></h2>");
+});
+app.post('/sign_up', (request, response) => {
+
+    const username = request.body.username;
+    const password = request.body.password;
+
+    if (!username || !password) {
+        // response что за объект?
+        response.status(400).end();
+        console.log("!username || !password");
+        return;
+    }
+    if (users[username]) {
+        // как добавить текст в ответ?
+        response.status(400).end();
+        return;
+    }
+
+    const new_id = idCreator();
+    users[username] = password;      // его вроде хешировать, по хорошему, надо?
+    ids[new_id] = username;
+
+
+    response.cookie(
+        'my_cookie', new_id,                        // Название и значение куки
+        { expires: new Date(Date.now() + 60 * 5) }  // Время истечения
+    );
+    response.status(200).end();
 });
 
 app.listen(process.env.PORT || 8080, function () {
