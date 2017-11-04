ae23739c987c4cb59d2c3814da4e13ba8aab1957
AndersRichter <andreysav96@rambler.ru>
2017-11-04 11:35:57 +0300

    game on new fetch + game-service

---
+++ develop/Game/game-service.js
@@ -0,0 +1,73 @@
+'use strict';
+
+import Http from "../modules/http";
+
+export default class GameService {
+    constructor() {
+        this.response = {
+            ok: false,
+            json: {},
+            message: "",
+        };
+    }
+
+
+    async start(width, height, maxPlayers) {
+        const resp = await Http.FetchPost('/game/single/create', {width, height, maxPlayers});
+        this.response.json = await resp.json();
+
+        if (resp.status >= 400) {
+            this.response.message = this.response.json.errorMessage;
+            return this.response;
+        }
+
+        this.response.ok = true;
+        this.user = this.response.json;
+        return this.response;
+    }
+
+
+    async complete(gameID) {
+        const resp = await Http.FetchGet('/game/complete?gameID=' + gameID);
+        this.response.json = await resp.json();
+
+        if (resp.status >= 400) {
+            this.response.message = this.response.json.errorMessage;
+            return this.response;
+        }
+
+        this.response.ok = true;
+        this.user = this.response.json;
+        return this.response;
+    }
+
+
+    async play(x1, x2, y1, y2, gameID, playerID, currentPlayerID) {
+        const resp = await Http.FetchPost('/game/play', {x1, x2, y1, y2, gameID, playerID, currentPlayerID});
+        this.response.json = await resp.json();
+
+        if (resp.status >= 400) {
+            this.response.message = this.response.json.errorMessage;
+            return this.response;
+        }
+
+        this.response.ok = true;
+        this.user = this.response.json;
+        return this.response;
+    }
+
+
+    async status(gameID, playerID, currentPlayerID) {
+        const resp = await Http.FetchPost('/game/status', {gameID, playerID, currentPlayerID});
+        this.response.json = await resp.json();
+
+        if (resp.status >= 400) {
+            this.response.message = this.response.json.errorMessage;
+            return this.response;
+        }
+
+        this.response.ok = true;
+        this.user = this.response.json;
+        return this.response;
+    }
+}
