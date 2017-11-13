e7f9419bf21e85038bd4eefec2f30a670d50a895
ed_grolsh <Drujininii@yandex.ru>
2017-11-14 02:13:41 +0300

    Lobby with data

--- develop/main/views/lobbyView/__lobbyFields/__gameDataField/__fields/lobbyView__lobbyFields__gameDataField__fields.js
+++ develop/main/views/lobbyView/__lobbyFields/__gameDataField/__fields/lobbyView__lobbyFields__gameDataField__fields.js
@@ -11,17 +11,17 @@ export default class GameDataFields {
 	constructor() {
 		this.fields = {
 			gameId: Block.Create('div', {}, ['gameId',
-				'lobbyView__lobbyFields__gameDataField__fields'], 'gameId = ///'),
+				'lobbyView__lobbyFields__gameDataField__fields'], 'gameId = 32'),
 			playersNumber: Block.Create('div', {}, ['playersNumber',
-				'lobbyView__lobbyFields__gameDataField__fields'], 'Players: ///'),
+				'lobbyView__lobbyFields__gameDataField__fields'], 'Players: 2'),
 			botsNumber: Block.Create('div', {}, ['botsNumber',
-				'lobbyView__lobbyFields__gameDataField__fields'], 'Bots: ///'),
+				'lobbyView__lobbyFields__gameDataField__fields'], 'Bots: 2'),
 			totalPLayersNumber: Block.Create('div', {}, ['totalPLayersNumber',
-				'lobbyView__lobbyFields__gameDataField__fields'], 'Total players: ///'),
+				'lobbyView__lobbyFields__gameDataField__fields'], 'Total players: 4'),
 			voyeursNumber: Block.Create('div', {}, ['voyeursNumber',
-				'lobbyView__lobbyFields__gameDataField__fields'], 'Voyeurs: ///'),
+				'lobbyView__lobbyFields__gameDataField__fields'], 'Voyeurs: 0'),
 			fieldSize: Block.Create('div', {}, ['filedSize',
-				'lobbyView__lobbyFields__gameDataField__fields'], 'Field size: ///'),
+				'lobbyView__lobbyFields__gameDataField__fields'], 'Field size: 10x10'),
 			// voyeurButton: ViewButton.Create({href: '/game'}, ['auth'], 'Play');
 			playButton: ViewButton.Create({href: '/gameInfo'}, ['playButton',
 				'lobbyView__lobbyFields__gameDataField__fields'], 'Play'),
