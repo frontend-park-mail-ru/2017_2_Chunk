16e41fb47484a1083d82e771c58bdc27be5fa31c
Anders Richter <andreysav96@rambler.ru>
2017-11-16 16:40:37 +0300

    3D + front

---
+++ develop/main/game3D/models/player.js
@@ -0,0 +1,33 @@
+'use strict';
+import * as THREE from 'three';
+import * as tools from '../tools/tools.js';
+
+export default class Player {
+	constructor(x = 0, z = 0, colorpl = 1) {
+		this.geometry = new THREE.CylinderGeometry(
+			0,
+			Math.min(tools.PLANE_X, tools.PLANE_Z) * 0.75 / 2,
+			tools.PLAYER_HEIGHT
+		);
+		if (colorpl === 1) {
+			this.material = new THREE.MeshLambertMaterial({
+				color: tools.COLORS.PLAYER_1
+			});
+			this.color = 1;
+		} else {
+			this.material = new THREE.MeshLambertMaterial({
+				color: tools.COLORS.PLAYER_2
+			});
+			this.color = 2;
+		}
+		this.mesh = new THREE.Mesh(this.geometry, this.material);
+		this.mesh.position.set(
+			(x + 0.5)*tools.PLANE_X,
+			tools.PLAYER_HEIGHT / 2 * 1.05,
+			(z + 0.5)*tools.PLANE_Z
+		);
+
+		this.x = x;
+		this.z = z;
+	}
+}
---
+++ develop/main/game3D/tools/tools.js
@@ -0,0 +1,25 @@
+'use strict';
+
+export const BACKGROUND_COLOR = 0xEEEEEE;
+export const HOVER_COLOR = 0xff0000;
+export const PLANE_X = 5;
+export const PLANE_Z = 5;
+export const PLAYER_HEIGHT = 7;
+export const PLANE_SIZE = 8;
+export const COLORS = {
+	RED:    0x8B0000,
+	GREEN:  0x32CD32,
+	AQUA:   0x7FFFD4,
+	YELLOW: 0xFFFF00,
+	BLUE:   0x00008B,
+	BLACK:  0x000000,
+	WHITE:  0xFFFFFF,
+	SNOW:   0xFFFAFA,
+	MILK:   0xEEEEEE,
+	L_BLUE: 0xA8D8FF,
+
+	BACKGROUND: 0xF4DE9E,
+	PLANE_COLOR: 0x69869D,
+	PLAYER_1:   0x7A6014,
+	PLAYER_2:   0x492C70
+};
