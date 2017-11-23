7f87db5d5ddffc7e00b8c12e12e1687524a3c6c3
ed_grolsh <Drujininii@yandex.ru>
2017-11-24 01:27:07 +0300

    heroku 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 'use strict';
-const path = require('path');
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const NODE_ENV = process.env.NODE_ENV || 'development';
