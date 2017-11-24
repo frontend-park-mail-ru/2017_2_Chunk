71b82dc2f4d39020f1a40ec9833fce2d4424073b
ed_grolsh <Drujininii@yandex.ru>
2017-11-24 09:50:22 +0300

    service worker

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -4,6 +4,8 @@ self.addEventListener('install', (event) => {
             .then((cache) => {
                 console.log('cache open');
                 return cache.addAll([
+                    './galaxy2-a81f392a9671ed0258e1899a3986505a.jpg',
+                    './galaxy2',
                     'menu',
                     'menu/application.css',
                     'menu/application.js',
