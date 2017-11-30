efe5f82611cb687157b0b198c531ee184cc962f6
ed_grolsh <Drujininii@yandex.ru>
2017-11-30 20:32:22 +0300

    music without controls

--- develop/loading/loading.js
+++ develop/loading/loading.js
@@ -38,7 +38,7 @@ new class StartLoader {
 		const script = document.createElement('script');
 		setTimeout(() => {
 			script.src = 'application.js';
-		}, 5000);
+		}, 8000);
 		document.body.appendChild(script);
 	}
 };
--- develop/main/services/loaders/loaderHtml.js
+++ develop/main/services/loaders/loaderHtml.js
@@ -7,7 +7,7 @@ const loaderHtml = '<div id="audio-control">\n' +
 	'</div>\n' +
 	'<div id="backgroundaudio">\n' +
 	' <i class="icon-volume-up"></i>\n' +
-	' <audio autoplay="" controls="" loop="" preload="" id="audio">\n' +
+	' <audio autoplay="" loop="" preload="" id="audio">\n' +
 	'  <source src="./music/Hooked on a Feeling.mp3" type="audio/mpeg"></source>\n' +
 	'  Your browser does not support the audio element.\n' +
 	' </audio>\n' +
