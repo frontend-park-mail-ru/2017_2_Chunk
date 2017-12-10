c78da832e32a78e64e29ddc9d726814f0ab5005e
AndersRichter <andreysav96@rambler.ru>
2017-12-10 19:14:50 +0300

    lalala

--- develop/main/game3D/draw.js
+++ develop/main/game3D/draw.js
@@ -273,7 +273,7 @@ export default class Draw {
 							code: '201',
 							step: {
 								src: this.point1,
-								dst: this.point2
+								dst: this.point2 //лалала
 							}
 						};
 						this.bus.emit(`${gameCodes.gameStep.request}`, request);
